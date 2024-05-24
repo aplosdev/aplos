b31b463a20c1c3640c1922095b64fcb2284a78fa
Gabriel Cozma <me@gabs.eu.org>
2024-05-24 17:59:20 +0300

    feat: Restyle blog list

--- layouts/ArticleList.vue
+++ layouts/ArticleList.vue
@@ -1,33 +1,45 @@
 <template>
-  <div class="blog-posts">
-    <noscript>
-      <small>To use the filter you need JavaScript to be enabled.</small>
-    </noscript>
-    <div class="filter-tags">
-      <button @click="filterPosts('')" id="all-tags">All</button>
-      <button v-for="tag in uniqueTags" :key="tag" @click="filterPosts(tag)">
-        <span class="hashtag">#</span>{{ tag }}
-      </button>
-    </div>
-    <div class="post-container">
-      <a
-        v-for="post in filteredPosts"
-        :key="post.title"
-        :href="`posts/${post.title.toLowerCase().replace(/\s+/g, '-')}.html`"
-        class="post"
-      >
-        <h3>{{ post.title }}</h3>
-        <p class="date">{{ post.date }}</p>
-        <p>{{ post.description }}</p>
-        <div class="tags">
-          <span v-if="typeof post.tags === 'string'" :key="post.tags"
-            >#{{ post.tags }}</span
-          >
-          <span v-else v-for="tag in post.tags" :key="tag">#{{ tag }}</span>
-        </div>
-      </a>
-    </div>
-  </div>
+	<div class="blog-posts">
+		<noscript>
+			<small>To use the filter you need JavaScript to be enabled.</small>
+		</noscript>
+		<div class="filter-tags">
+			<button @click="filterPosts('')" id="all-tags">All</button>
+			<button
+				v-for="tag in uniqueTags"
+				:key="tag"
+				@click="filterPosts(tag)"
+			>
+				<span class="hashtag">#</span>{{ tag }}
+			</button>
+		</div>
+		<div class="post-container">
+			<article
+				v-for="post in filteredPosts"
+				:key="post.title"
+				class="post"
+			>
+				<h3>
+					<a
+						:href="`posts/${post.title
+							.toLowerCase()
+							.replace(/\s+/g, '-')}.html`"
+						>{{ post.title }}</a
+					>
+				</h3>
+				<p class="date">{{ post.date }}</p>
+				<p>{{ post.description }}</p>
+				<div class="tags">
+					<span v-if="typeof post.tags === 'string'" :key="post.tags"
+						>#{{ post.tags }}</span
+					>
+					<span v-else v-for="tag in post.tags" :key="tag"
+						>#{{ tag }}</span
+					>
+				</div>
+			</article>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -37,127 +49,120 @@ import { data as posts } from "../../../../../../pages/.vitepress/theme/posts.da
 const selectedTag = ref(null);
 
 const allTags = computed(() => {
-  return posts.reduce((tags, post) => {
-    return tags.concat(Array.isArray(post.tags) ? post.tags : [post.tags]);
-  }, []);
+	return posts.reduce((tags, post) => {
+		return tags.concat(Array.isArray(post.tags) ? post.tags : [post.tags]);
+	}, []);
 });
 
 const uniqueTags = computed(() => {
-  const tags = [...new Set(allTags.value)];
-  return tags.filter((tag) => tag !== "");
+	const tags = [...new Set(allTags.value)];
+	return tags.filter((tag) => tag !== "");
 });
 
 const filteredPosts = computed(() => {
-  return selectedTag.value === null
-    ? posts
-    : posts.filter((post) =>
-        Array.isArray(post.tags)
-          ? post.tags.includes(selectedTag.value)
-          : post.tags === selectedTag.value
-      );
+	return selectedTag.value === null
+		? posts
+		: posts.filter((post) =>
+				Array.isArray(post.tags)
+					? post.tags.includes(selectedTag.value)
+					: post.tags === selectedTag.value
+		  );
 });
 
 function filterPosts(tag: string) {
-  selectedTag.value = tag === "" ? null : tag;
+	selectedTag.value = tag === "" ? null : tag;
 }
 </script>
 
 <style lang="scss">
 .blog-posts {
-  margin: 0 auto;
+	margin: 0 auto;
 }
 
 .post-container {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(100%, 1fr));
-  gap: 10px;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(100%, 1fr));
+	gap: 15px;
 
-  @media screen and (max-width: 600px) {
-    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  }
+	@media screen and (max-width: 600px) {
+		grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+	}
 
-  .post {
-    background-color: var(--color-background-second);
-    text-decoration: none;
-    padding: 20px;
-    border-radius: 8px;
-    transition: transform 0.3s ease;
+	.post {
+		background-color: var(--color-background-second);
+		padding: 20px;
+		border-radius: 8px;
+		transition: transform 0.3s ease;
 
-    &:first-child {
-      background-color: var(--color-background-mute);
-    }
+		&:first-child {
+			background-color: var(--color-background-mute);
+		}
 
-    &:hover {
-      transform: scale(1.05);
-    }
+		h3 {
+			margin: 0 !important;
+			text-decoration: underline var(--color-accent) 1px solid;
+			font-weight: 900;
+			font-size: 24px;
+		}
 
-    &:active {
-      transform: scale(0.9);
-    }
+		p {
+			margin: 0;
+			color: var(--color-text);
+      text-decoration: none;
+			font-weight: 500;
+		}
 
-    h3 {
-      margin: 0 !important;
-      font-weight: 900;
-      font-size: 24px;
-    }
-
-    p {
-      margin: 0;
-      color: var(--color-text);
-      font-weight: 500;
-    }
-
-    .date {
-      margin: 5px 0;
-      font-size: 0.9rem;
-      color: var(--color-text-secondary);
-      font-weight: 600;
-      font-feature-settings: "zero", "tnum", "cv03", "cv02";
-    }
-  }
+		.date {
+			margin: 5px 0;
+			font-size: 0.9rem;
+			color: var(--color-text-secondary);
+			font-weight: 600;
+			font-feature-settings: "zero", "tnum", "cv03", "cv02";
+		}
+	}
 }
 
 .filter-tags {
-  margin-bottom: 20px;
+	margin-bottom: 20px;
 
-  #all-tags {
-    background-color: var(--color-accent-alpha);
-    color: var(--color-accent)
-  }
+	#all-tags {
+		background-color: var(--color-accent-alpha);
+		color: var(--color-accent);
+	}
 
-  button {
-    margin-right: 10px;
-    margin-bottom: 3px;
-    background-color: var(--color-background-second);
-    border-radius: 20px;
-    padding: 0.25rem 0.75rem;
-    font-size: 0.8rem;
-    cursor: pointer;
-    transition: all 0.3s ease;
+	button {
+		margin-right: 10px;
+		margin-bottom: 3px;
+		background-color: var(--color-background-second);
+		border-radius: 20px;
+		padding: 0.25rem 0.75rem;
+		font-size: 0.8rem;
+		cursor: pointer;
+		transition: all 0.3s ease;
 
-    &:hover {
-      color: var(--color-accent);
-      background-color: var(--color-accent-alpha);
-    }
+		&:hover {
+			color: var(--color-accent);
+			background-color: var(--color-accent-alpha);
+		}
 
-    &:active {
-      transform: scale(0.8);
-    }
-  }
+		&:active {
+			transform: scale(0.8);
+		}
+	}
 }
 
 .tags {
-  text-align: right;
-  margin-top: 10px;
+	text-align: right;
+	margin-top: 10px;
 
-  span {
-    font-weight: 500;
-    margin-right: 5px;
-    color: var(--color-text-accent);
-    background-color: var(--color-background);
-    padding: 0.15rem 0.45rem;
-    border-radius: 20px;
-    font-size: 12px;
-  }
+	span {
+		font-weight: 500;
+		margin-right: 5px;
+		color: var(--color-text-accent);
+		background-color: var(--color-background);
+		padding: 0.15rem 0.45rem;
+		border-radius: 20px;
+		font-size: 12px;
+	}
 }
 </style>
