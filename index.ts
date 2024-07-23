7b9bf1af65f64c47d9788a8492236a13176878c6
Gabriel Cozma <me@gabs.eu.org>
2024-07-23 12:58:03 +0300

    feat: migrate the plugins to an NPM package

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import "./src/styles/fonts.scss";
 import "./src/styles/index.scss";
 import "@csstools/normalize.css";
+import "@aplosdev/plugins";
 import "inter-ui/inter-variable.css";
 import "inter-ui/inter.css";
 import "non.geist/mono";
