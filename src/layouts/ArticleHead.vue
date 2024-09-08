c66956480d4d24b119b7154b7569c84a9179ac6f
Gabriel Cozma <me@gabs.eu.org>
2024-09-08 17:31:20 +0300

    cleanup: new date format

--- src/layouts/ArticleHead.vue
+++ src/layouts/ArticleHead.vue
@@ -33,8 +33,6 @@
               day: "2-digit",
               month: "long",
               year: "numeric",
-              hour: "2-digit",
-              minute: "2-digit",
             },
           )
         }}
